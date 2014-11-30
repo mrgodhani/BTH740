d277d24a11d6b7fce4d1b2bf3a312fbc800046fa
Meet Godhani <meet_godhani1@yahoo.com>
2014-11-30 15:49:19 -0500

    bread is done

--- Js/controllers.js
+++ Js/controllers.js
@@ -4,6 +4,14 @@
 
 angular.module('myApp').
     controller('OrderController',
-    function() {
-
+    function($scope)
+    {
+        $scope.breadtypes = [
+            {name:'Italian', image: 'https://order.subway.com/Images/Subway/en-us/Options/o_BreadItalian.png'},
+            {name:'9-Grain Wheat', image: 'https://order.subway.com/Images/Subway/en-us/Options/o_BreadWheat.png'},
+            {name:'Honey Oat', image: 'https://order.subway.com/Images/Subway/en-us/Options/o_BreadHOWheat.png'},
+            {name:'Italian Herb & Cheese', image: 'https://order.subway.com/Images/Subway/en-us/Options/o_BreadItalianHerb.png'},
+            {name:'Flat Bread', image: 'https://order.subway.com/Images/Subway/en-us/Options/o_BreadFlat.png'},
+            {name:'Jalapeno Cheese', image: 'https://order.subway.com/Images/Subway/en-us/Options/o_BreadJalepenoCheese.png'}
+        ];
     });
